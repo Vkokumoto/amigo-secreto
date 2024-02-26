7be1d295a1d42c62edd7a2d141cb06ac80640b1e
Victor Okumoto <okumotovictor@gmail.com>
2024-02-26 17:26:28 -0300

    implementar função reiniciar()

--- js/app.js
+++ js/app.js
@@ -41,5 +41,7 @@ function embaralha(lista) {
 }
 
 function reiniciar() {
-
+    amigos = [];
+    document.getElementById('lista-amigos').innerHTML = '';
+    document.getElementById('lista-sorteio').innerHTML = '';
 }
